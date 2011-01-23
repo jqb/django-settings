ff9bbe79d7a5b5fd19ab533591ef99626becf939
Trey Hunner <treyhunner@gmail.com>
2011-01-22 19:18:55 -0800

    PEP8

--- django_settings/management.py
+++ django_settings/management.py
@@ -6,16 +6,14 @@ from django.contrib.contenttypes.models import ContentType
 from django_settings import models
 
 
-
 DEFAULT_SETTINGS = getattr(settings, 'DJANGO_SETTINGS', {})
-
 
 
 def initialize_data(sender, **kwargs):
     for name, type_name_and_value in DEFAULT_SETTINGS.items():
         type_name, value = type_name_and_value
         SettingClass = ContentType.objects.get(app_label='django_settings', model=type_name.lower()).model_class()
-        
+
         if not models.Setting.objects.value_object_exists(name):
             models.Setting.objects.set_value(name, SettingClass, value)
 
