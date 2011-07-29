357699f0b72c234b8a23731bba90b841cd7ba02e
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-07-29 15:42:19 +0200

    Test fix.

--- django_settings/tests.py
+++ django_settings/tests.py
@@ -4,19 +4,12 @@ from django.test import TestCase
 
 from django_settings.models import Setting
 
-
 DJANGO_SETTINGS = settings.DJANGO_SETTINGS
 
 
 class SettingDefaults(TestCase):
-    def test_default_settings(self):
-        """
-        Test assuemes that following dict is in your settings.py file:
-
-        DJANGO_SETTINGS = {
-           'application_limit': ('Integer', 2),
-           'admin_email': ('String', 'kuba.janoszek@gmail.com'),
-           }
-        """
-        value = Setting.objects.get_value('application_limit')
-        self.assertEquals(value, DJANGO_SETTINGS['application_limit'][1])
+    def test_settings(self):
+        """Test assumes that a properly formatted DJANGO_SETTINGS dict is in your settings.py"""
+        for key in DJANGO_SETTINGS:
+            value = Setting.objects.get_value(key)
+            self.assertEquals(value, DJANGO_SETTINGS[key][1])
