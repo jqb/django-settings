99687e6a822b0db483c668cbb4b41845f8be902d
jqb <kuba.janoszek@gmail.com>
2013-08-31 22:09:37 +0200

    setup updates: version replaced with hardcoded string

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-import re
 from setuptools import setup, find_packages
 
-version = re.search(
-    r"VERSION = ['\"](.*)['\"]",
-    open("django_settings/__init__.py").read()
-).group(1)
 
 setup(
     name='django-settings',
-    version=version,
+    version='1.3.4',
     description='Simple django reusable application for storing project settings in database.',
     author='Kuba Janoszek',
     author_email='kuba.janoszek@gmail.com',
