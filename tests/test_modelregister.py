23cedeb486741d2b5a7523e55d1fe3a8b3fb23ce
jqb <kuba.janoszek@gmail.com>
2013-10-05 20:00:39 +0200

    tests: removing "n" helper (step 2) - test_modelregister cleanup

--- tests/test_modelregister.py
+++ tests/test_modelregister.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 # test stuff
-from . import TestCase, n
+from . import TestCase
 
 
 class ModelRegisterTest(TestCase):
     def test_importing_special_module_should_add_all_registered_classes(self):
         # before special module import
         from .moduleregistry_testapp import models
-        n.assert_false(hasattr(models, 'MyString'))
-        n.assert_false('MyString' in models.registry)
+        self.assert_false(hasattr(models, 'MyString'))
+        self.assert_false('MyString' in models.registry)
 
         # import special module that triggers registering
         from .moduleregistry_testapp import settingsmodels
         settingsmodels.register()
 
-        n.assert_true(hasattr(models, 'MyString'))
-        n.assert_true('MyString' in models.registry)
+        self.assert_true(hasattr(models, 'MyString'))
+        self.assert_true('MyString' in models.registry)
 
-        n.assert_not_equal(
+        self.assert_not_equal(
             models.MyString.__module__,
             settingsmodels.MyString.__module__,
         )
 
         # technicaly those models are not the same
-        n.assert_not_equal(models.MyString, settingsmodels.MyString)
-        n.assert_true(issubclass(models.MyString, settingsmodels.MyString))
+        self.assert_not_equal(models.MyString, settingsmodels.MyString)
+        self.assert_true(issubclass(models.MyString, settingsmodels.MyString))
 
         # registered class became a part of the module
-        n.assert_equal(models.MyString.__module__, models.__name__)
+        self.assert_equal(models.MyString.__module__, models.__name__)
 
         models.registry.unregister('MyString')
-        n.assert_false(hasattr(models, 'MyString'))
+        self.assert_false(hasattr(models, 'MyString'))
 
