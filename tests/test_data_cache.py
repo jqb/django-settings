f359039a1a5294c46f2b5d4be0400eb1351296a0
jqb <kuba.janoszek@gmail.com>
2013-10-05 19:51:45 +0200

    tests: dataapi tests refactorisation and fixes
    Previous version of assertion that was counting queries was not work
    as expected. Also more test has been added and futher simplifications
    has been applied.

--- tests/test_data_cache.py
+++ tests/test_data_cache.py
@@ -6,32 +6,44 @@ from . import DBTestCase, n
 import django_settings
 
 
-def assure_db_queries(function, num):
-    from django import db
-
-    before = len(db.connection.queries)
-    result = function()
-    after  = len(db.connection.queries)
-
-    queries_count = after - before
-    if queries_count != num:
-        raise Exception("Number of queries: %d, but expected: %s" % (queries_count, num))
-
-    return result
-
-
 class DataCachingTest(DBTestCase):
-    def before_database_setup(self):
+    def setup(self):
         django_settings.data.clear_cache()
-        from moduleregistry_testapp import settingsmodels
 
     def teardown(self):
-        from moduleregistry_testapp import models
-        models.registry.unregister_all()
+        django_settings.data.clear_cache()
 
     def test_contenttypes_names_should_be_cached(self):
-        django_settings.data.clear_cache()
-        # NOTE: it clears ALL the cache not only related to django_settings
+        def get_contenttypes_names():
+            django_settings.data.contenttypes_names()
+        self.assert_queries_count(1, get_contenttypes_names)
 
-        assure_db_queries(lambda: django_settings.data.contenttypes_names(), 0)
+    def test_should_run_queries_only_for_save(self):
+        def setting_up_setting():
+            cached_value = django_settings.data.set('Integer', 'test-1', 1)
+            n.assert_equal(cached_value, 1)
 
+        def getting_setting():
+            cached_value = django_settings.data.get('test-1')
+            n.assert_equal(cached_value, 1)
+
+        def checking_existence_of_setting():
+            exists = django_settings.data.exists('test-1')
+            n.assert_true(exists)
+
+        # django should run 4 queries for us here:
+        # 1) check if *setting* object exists (if yes -
+        #    delete it, but it's not the case here)
+        # 2) check if *setting.setting_object* exists
+        # 3) create new *setting.setting_object* object
+        # 4) create new *setting* object
+        self.assert_queries_count(4, setting_up_setting)
+
+        # and then getting and checking existence of setting takes no queries
+        self.assert_queries_count(0, getting_setting)
+        self.assert_queries_count(0, checking_existence_of_setting)
+
+        # ...even if you ask many times...
+        for _ in xrange(10):
+            self.assert_queries_count(0, getting_setting)
+            self.assert_queries_count(0, checking_existence_of_setting)
