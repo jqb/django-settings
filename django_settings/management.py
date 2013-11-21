72acf0e93c8b2a4bc8dd0c78fa1c703e63b7a26f
Boris Savelev <boris.savelev@gmail.com>
2013-11-21 19:41:19 +0400

    #14

--- django_settings/management.py
+++ django_settings/management.py
@@ -11,7 +11,7 @@ def initialize_data(sender, **kwargs):
     for name, type_name_and_value in DEFAULT_SETTINGS.items():
         type_name, value = type_name_and_value
 
-        if not dataapi.data.exists(type_name):
+        if not dataapi.data.exists(name):
             dataapi.data.set(type_name, name, value)
 
 signals.post_syncdb.connect(initialize_data, sender=models)
