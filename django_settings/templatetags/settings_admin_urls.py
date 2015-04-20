c2e69daf9172dafd7158ac3d35c07280baea5ef9
jperelli <jperelli@gmail.com>
2015-04-20 20:09:30 -0300

    variable not used

--- django_settings/templatetags/settings_admin_urls.py
+++ django_settings/templatetags/settings_admin_urls.py
@@ -10,8 +10,8 @@ register = template.Library()
 @register.filter
 def add_url_for_setting_type(admin_change_list, type_name):
     cl = admin_change_list
-    param2 = cl.opts.module_name if django.VERSION < (1, 5) else cl.opts.model_name
-    url_name = 'admin:%s_%s_%s' % (cl.opts.app_label, cl.opts.module_name, 'add')
+    param2 = cl.opts.module_name if django.VERSION < (1, 6) else cl.opts.model_name
+    url_name = 'admin:%s_%s_%s' % (cl.opts.app_label, param2, 'add')
     query = "typename=%(type)s%(popup)s" % dict(
         type=type_name,
         popup='_popup=1' if cl.is_popup else '',
