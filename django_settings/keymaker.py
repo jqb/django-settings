783f7a5d17b3db83e1f27ad3bebb4c165c4e66ca
Juho Rutila <juho.rutila@iki.fi>
2015-12-28 22:57:06 +0200

    Fix convert to support python 2 and python 3

--- django_settings/keymaker.py
+++ django_settings/keymaker.py
@@ -1,8 +1,12 @@
+import sys
+
 class KeyMaker(object):
     def __init__(self, prefix):
         self.prefix = prefix
 
     def convert(self, arg):
+        if sys.version_info < (3,) and isinstance(arg, unicode):
+            return arg.encode(django.settings.DEFAULT_CHARSET)
         return str(arg)
 
     def args_to_key(self, args):
