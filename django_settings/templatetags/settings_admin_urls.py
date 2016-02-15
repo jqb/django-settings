51bb48e4f253e7c0cf3629022abece4d0be04c04
dnaranjo89 <dnaranjo89@gmail.com>
2016-02-15 11:29:40 +0100

    Fixup cl.opts.module_name
    Ranamed to cl.opts.model_name in Django 1.8

--- django_settings/templatetags/settings_admin_urls.py
+++ django_settings/templatetags/settings_admin_urls.py
@@ -8,7 +8,11 @@ register = template.Library()
 @register.filter
 def add_url_for_setting_type(admin_change_list, type_name):
     cl = admin_change_list
-    url_name = 'admin:%s_%s_%s' % (cl.opts.app_label, cl.opts.module_name, 'add')
+    try:
+        model_name = cl.opts.model_name
+    except AttributeError:
+        model_name = cl.opts.module_name
+    url_name = 'admin:%s_%s_%s' % (cl.opts.app_label, model_name, 'add')
     query = "typename=%(type)s%(popup)s" % dict(
         type=type_name,
         popup='_popup=1' if cl.is_popup else '',
